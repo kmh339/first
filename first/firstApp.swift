6c20c80b79f44371f75d854f1f754ab11add8ec5
kmh339 <kmh339@gmail.com>
2021-01-23 19:42:01 +0900

    Initial Commit

---
+++ first/firstApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstApp.swift
+//  first
+//
+//  Created by red on 2021/01/23.
+//
+
+import SwiftUI
+
+@main
+struct firstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
