a4f88d58b2f9c21bd1334b78ef7ee4dd786a2e06
kmh339 <kmh339@gmail.com>
2021-01-29 01:05:29 +0900

    add bookmark button in details

--- first/Views/ContentView.swift
+++ first/Views/ContentView.swift
@@ -13,10 +13,9 @@ struct ContentView: View {
     }
 }
 
-#if DEBUG
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environmentObject(ModelData())
     }
 }
-#endif
